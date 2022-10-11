b05ed330dd841570d5cf52974504a0ac7fb50228
Han Lee <hanlee4g@gmail.com>
2022-10-11 12:08:57 -0500

    search bar style

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import './SearchBar.css'
 const SearchBar = () => (
     <div className="search-container">
         <input type="text" placeholder="Ask for advice..."></input>
-        <button type="submit">Submit</button>
+        <button className='submit-button' type="submit">Submit</button>
     </div>
 );
 
