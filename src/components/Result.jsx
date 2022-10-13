4aea470f35ca616361fc08241494449b152f3f55
dyu222 <48658466+dyu222@users.noreply.github.com>
2022-10-13 12:32:15 -0500

    Results component start

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,6 +1,10 @@
+import './Result.css'
+
 const Result = () => (
-    <div>
-        Result
+    <div class="row">
+        <div class="column left"> Column 1</div>
+        <div class="column middle"> Column 2</div>
+        <div class="column right"> Column 3</div>
     </div>
 );
 
