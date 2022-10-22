217f870d5e480df8634a288361678bfe68d944aa
ChanhBuiNorthwestern <chanh.bui@northwestern.edu>
2022-10-22 11:40:32 -0500

    First commit for reddit API. Default example.

--- src/App.js
+++ src/App.js
@@ -13,4 +13,34 @@ function App() {
   );
 }
 
+document.addEventListener("DOMContentLoaded", function(){
+  redditResponse("Test");
+});
+
+function redditResponse(s){
+  var snoowrap = require('snoowrap');
+  const r = new snoowrap({
+    userAgent: 'Multi-vise',
+    clientId: 'g0mFlCzhimudIRrqxZuqVw',
+    clientSecret: 'feYFQImHDx42cW-3ce1Bq18GtysnwQ',
+    refreshToken: '1972848092863-Le8seeRHjHt_40HFLrIC10yNaAWvPg'
+  });
+
+  const subreddit = r.getSubreddit('realEstate');
+  const topPosts = subreddit.getTop({time: 'week', limit: 3});
+
+  let data = [];
+
+  topPosts.forEach((post) => {
+    data.push({
+      link: post.url,
+      text: post.title,
+      score: post.score
+    })
+  });
+  
+  console.log(data);
+  console.log(s);
+}
+
 export default App;
