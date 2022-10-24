85abbe0dfc35ba5e94ed617346d1b04cad9b620c
dyu222 <48658466+dyu222@users.noreply.github.com>
2022-10-24 00:52:43 -0500

    receive value in search bar

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,10 @@
 import './Result.css'
 
 const Result = () => (
-    <div class="row">
-        <div class="column left"> Column 1</div>
-        <div class="column middle"> Column 2</div>
-        <div class="column right"> Column 3</div>
+    <div className="row">
+        <div className="column left"> Column 1</div>
+        <div className="column middle"> Column 2</div>
+        <div className="column right"> Column 3</div>
     </div>
 );
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,21 @@
 import './SearchBar.css'
 
+function getVal(){
+    var userQuestion = document.getElementById('questionField').value;
+    // var question = document.getElementById("questionField");
+    // this.questionField = ''; // only needed maybe if we keep search bar after submit
+    console.log(userQuestion);
+} //need to funnel userQuestion into the API call now i think??
+
 const SearchBar = () => (
     <div className="search-container">
-        <input type="text" placeholder="Ask for advice..."></input>
-        <button className='submit-button' type="submit">Submit</button>
+        <input id="questionField" type="text" placeholder="Ask for advice..."></input>
+        {/* <button className='submit-button' type="submit" Onclick={this.getVal}>Submit</button> */}
+        {/* <button id="subButton" className='submit-button' type="submit" onlick='getVal()'>Submit</button> */}
+        <button id="subButton" className='submit-button' type="submit" onClick={getVal}>Submit</button>
     </div>
 );
+// var subButton = document.getElementById('subButton');
+// subButton.addEventListener('click', getVal, false); 
 
 export default SearchBar;
